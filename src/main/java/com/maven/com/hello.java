851628217093fc60640c170841004d0eb9cffaf7
MITHUNg6 <47732412+MITHUNg6@users.noreply.github.com>
2019-09-18 17:14:05 +0530

    hello file is modified

--- src/main/java/com/maven/com/hello.java
+++ src/main/java/com/maven/com/hello.java
@@ -2,6 +2,7 @@ class hello
 {
 public static void main (String args[])
 {
-System.out.println("welcome to hello maven projecy");
+System.out.println("welcome to hello maven project");
+  System.out.print("statement modified");
 }
 }
